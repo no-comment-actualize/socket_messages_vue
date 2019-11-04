0e36fd0142cd4ea887e21ae6bccf2a8fb13418a0
dani <dani.zaghian@gmail.com>
2019-11-04 09:46:36 -0800

    adds actioncable real time messaging

--- src/views/Messages.vue
+++ src/views/Messages.vue
@@ -26,6 +26,7 @@
 
 <script>
 import axios from "axios";
+import ActionCable from "actioncable";
 
 export default {
   data: function() {
@@ -38,6 +39,21 @@ export default {
     axios.get("/api/messages").then(response => {
       this.messages = response.data;
     });
+    var cable = ActionCable.createConsumer("ws://localhost:3000/cable");
+    cable.subscriptions.create("MessagesChannel", {
+      connected: () => {
+        // Called when the subscription is ready for use on the server
+        console.log("Connected to MessagesChannel");
+      },
+      disconnected: () => {
+        // Called when the subscription has been terminated by the server
+      },
+      received: data => {
+        // Called when there's incoming data on the websocket for this channel
+        console.log("Data from MessagesChannel:", data);
+        this.messages.unshift(data); // update the messages in real time
+      }
+    });
   },
   methods: {
     createMessage: function() {
@@ -47,7 +63,7 @@ export default {
 
       axios.post("/api/messages", params).then(response => {
         this.newMessageBody = "";
-        this.messages.unshift(response.data);
+        // this.messages.unshift(response.data);
       });
     }
   }
